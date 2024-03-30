487200cbfa9c4805c0b73acfac9ce9f93f077f42
Phrdavis <davipi_sou@hotmail.com>
2024-03-30 12:54:49 -0300

    DELETE Customer
    Delete method for delete a customer using @DeleteMapping

--- src/main/java/com/springboot/Control/CustomerController.java
+++ src/main/java/com/springboot/Control/CustomerController.java
@@ -2,7 +2,9 @@ package com.springboot.Control;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -52,4 +54,12 @@ public class CustomerController {
         customerRepository.save(customer);
 
     }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+
+        customerRepository.deleteById(id);
+
+    }
+    
 }
