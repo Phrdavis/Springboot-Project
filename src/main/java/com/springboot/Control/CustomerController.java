c98691b8e51271697c05d38eb447f09531343d73
Phrdavis <davipi_sou@hotmail.com>
2024-03-30 13:09:20 -0300

    UPDATE method
    UPDATE method add to the CustomerController @PutMapping

--- src/main/java/com/springboot/Control/CustomerController.java
+++ src/main/java/com/springboot/Control/CustomerController.java
@@ -2,10 +2,12 @@ package com.springboot.Control;
 
 import java.util.List;
 
+import java.util.Optional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,4 +64,20 @@ public class CustomerController {
 
     }
     
+    @PutMapping("{customerId}")
+    public void updateCustomer( @PathVariable("customerId") Integer id, 
+                                @RequestBody NewCustomerRequest request){
+
+        Optional<Customer> OptionalCustomer = customerRepository.findById(id);
+
+        Customer customer = OptionalCustomer.get();
+
+        customer.setName(request.name);
+        customer.setAge(request.age);
+        customer.setEmail(request.email);
+
+        customerRepository.save(customer);
+
+
+    }
 }
